7bdd6448ac0e838390211248c9887c61019d10db
Avnit2303 <gadhiyaavnit@gmail.com>
2025-06-29 21:51:17 +0530

    TMDB data fetch && build maincontainer

---
+++ src/componnet/Videotitle.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Videotitle = ({title,overview}) => {
+  return (
+    <div className='pt-[15%] px-11 absolute text-white bg-gradient-to-r from-black w-full aspect-video'>
+        <h1 className='text-4xl font-bold'>{title}</h1>
+         <p className='w-1/4 py-4'>{overview}</p>
+         <div>
+          </div>
+    </div>
+  )
+}
+
+export default Videotitle
